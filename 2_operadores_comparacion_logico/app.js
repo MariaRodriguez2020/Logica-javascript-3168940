3608f6a9f47261e37fdf176a3d1a4377e26c0be1
MariaRodriguez2020 <mariafernandarodriguez186@gmail.com>
2025-07-31 13:29:15 -0500

    Ejercicio Adelantado

---
+++ 2_operadores_comparacion_logico/app.js
@@ -0,0 +1,32 @@
+//boolear
+
+console.log("5 ==5", 5 == 5)//true
+console.log(' "5" === "5" ', "5" === "5" )
+console.log(' 5 === "5" ', 5 === "5" )
+console.log(' 5 == "5" ', 5 === "5" )
+console.log(' 5 === (3+2)  ', "5" === (3+2) )
+console.log(' 5 === 5 ', 5 === 5 )
+    
+
+//concatenar 
+
+console.log( 5 + "3" ) 
+console.log( 5 + Number(3))
+console.log( 5 * "3")
+console.log( 5 + "3")
+
+
+//Pida dos numeros al usuario y sumelos
+
+let num1 = number (prompt ('Dime un numero'))
+let num2 = number (prompt ('Dime un numero'))
+
+let suma = num1 + num2
+let resta = num1 - num2
+let multiplicacion = num1 *  num2
+let division = num1 / num2
+
+alert ('La suma de' + num1 + '+' + num2 + '=' + suma)
+alert ('La resta de ' + 
+
+
