a37f251cecae6da1c1752bc6137b166b72f56cc7
MariaRodriguez2020 <mariafernandarodriguez186@gmail.com>
2025-07-31 12:38:47 -0500

    repositorio js version 1

---
+++ logica javascript/1_SINTAXIS_VARIABLE/app.js
@@ -0,0 +1,26 @@
+//el alert muestra una ventana emergente en el navegador
+alert("HOLA MUNDO")
+//prompt muestra una caja para ingresar informacion
+prompt("dime tu nombre")
+
+//variabledeclarada con let
+let personajes ="perritos"
+//crear variavles puntajes y asignar valor de 0 
+//los valores numericos van sin comillas
+let puntaje = 0
+//crear variables shots y asignar valor de 3
+let shots = 3
+    recarga = 2
+
+console.log("total balas:", shots + recarga)
+
+//el usuario capturo 4 patos 
+puntaje += 4
+//el usuario gasto 2 balas 
+shots -=2
+//el usuario recargo su arma
+recarga += 1
+
+console.log("puntaje:", puntaje)
+console.log("shots:", shots)
+console.log("recarga:", recarga)
---
+++ logica javascript/2_operadores_comparacion_logico/app.js
@@ -0,0 +1,9 @@
+//boolear
+
+console.log("5 ==5", 5 == 5)//true
+console.log(' "5" === "5" ', "5" === "5" )
+console.log(' 5 === "5" ', 5 === "5" )
+console.log(' 5 == "5" ', 5 === "5" )
+console.log(' 5 === (3+2)  ', "5" === (3+2) )
+console.log(' 5 === 5 ', 5 === 5 )
+    
